63b6c0b28703ff947a9e46aa9e005a8d04573f16
ece <ece@deepvaio>
2017-01-20 17:06:56 +0100

    dev

--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -2,6 +2,7 @@ package sec.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ public class SignupController {
 
     @Autowired
     private SignupRepository signupRepository;
+    private int capacity = 3;
 
     @RequestMapping("*")
     public String defaultMapping() {
@@ -20,14 +22,37 @@ public class SignupController {
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.GET)
-    public String loadForm() {
+    public String loadForm(Model model) {
+        model.addAttribute("available", capacity - signupRepository.count() );
         return "form";
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
-    public String submitForm(@RequestParam String name, @RequestParam String address) {
-        signupRepository.save(new Signup(name, address));
-        return "done";
+    public String submitForm(@RequestParam String name, @RequestParam String email, @RequestParam String address, Model model) {
+        //check signup by email.
+        Signup s = signupRepository.findByEmail(email);
+        if (s == null) {
+            signupRepository.save(new Signup(name, email, address));
+            return "done";
+        } else {
+            model.addAttribute("user", s);
+            return "signup";
+        }
     }
-
+    
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String queryUsers(Model model) {
+        model.addAttribute("users", signupRepository.findAll());
+        return "users";
+    }
+    
+        @RequestMapping(value = "/user", method = RequestMethod.DELETE)
+    public String deleteUser(@RequestParam String email, @RequestParam(required=false) Boolean admin) {
+        signupRepository.delete(signupRepository.findByEmail(email));
+        if (admin != null)
+            return "redirect:/users";
+        else
+            return "redirect:/form";
+    }
+    
 }
