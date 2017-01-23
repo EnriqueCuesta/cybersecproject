0bb53918472270b50e6db6edb5c5446aed1ba29c
ece <ece@deepvaio>
2017-01-23 17:42:12 +0100

    Authentication and database.

--- src/main/java/sec/project/config/SecurityConfiguration.java
+++ src/main/java/sec/project/config/SecurityConfiguration.java
@@ -3,13 +3,13 @@ package sec.project.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.encoding.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -21,7 +21,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // no real security at the moment
-        http.authorizeRequests()
+//        http.authorizeRequests()
+//                .anyRequest().permitAll();
+        // you can access the h2-console through /h2-console
+        http.csrf().disable();
+        http.headers().frameOptions().sameOrigin();
+
+        http
+            .authorizeRequests()
                 .anyRequest().permitAll();
     }
 
@@ -32,6 +39,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new Md5PasswordEncoder();
     }
 }
--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -1,6 +1,12 @@
 package sec.project.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +18,9 @@ import sec.project.repository.SignupRepository;
 @Controller
 public class SignupController {
 
+    @Autowired
+    private HttpSession session;
+    
     @Autowired
     private SignupRepository signupRepository;
     private int capacity = 3;
@@ -42,17 +51,77 @@ public class SignupController {
     
     @RequestMapping(value = "/users", method = RequestMethod.GET)
     public String queryUsers(Model model) {
-        model.addAttribute("users", signupRepository.findAll());
-        return "users";
+        String userAuthenticated = (String) session.getAttribute("userauthenticated");
+        if (userAuthenticated != null) {
+            model.addAttribute("users", signupRepository.findAll());
+            model.addAttribute("userauth", userAuthenticated);
+            return "users";
+        } else {
+            return "redirect:/adminlogin";
+        }
+        
     }
     
         @RequestMapping(value = "/user", method = RequestMethod.DELETE)
-    public String deleteUser(@RequestParam String email, @RequestParam(required=false) Boolean admin) {
+    public String deleteUser(@RequestParam String email) {
         signupRepository.delete(signupRepository.findByEmail(email));
-        if (admin != null)
+        if (session.getAttribute("userauthenticated") != null)
             return "redirect:/users";
         else
             return "redirect:/form";
+    }
+    
+        @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public String modifyUser(@RequestParam String email, @RequestParam String name, @RequestParam String address, Model model) {
+        Signup s = signupRepository.findByEmail(email);
+        s.setName(name);
+        s.setAddress(address);
+        signupRepository.save(s);
+        
+        model.addAttribute("user", s);
+        return "signup";
+    }
+    
+    @RequestMapping(value = "/adminlogin", method = RequestMethod.GET)
+    public String loginForm() {
+        return "adminlogin";
+    }
+    
+    @RequestMapping(value = "/adminlogin", method = RequestMethod.POST)
+    public String adminLogin(@RequestParam String username, @RequestParam String password, Model model) {
+        
+        try {
+            // Open connection
+            Connection connection = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "");
+            
+            // Execute query and retrieve the query results
+            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM ACCOUNT WHERE USERNAME='"+username+"' and PASSWORD='"+(new Md5PasswordEncoder()).encodePassword(password, null)+"'");
+            
+            // Do something with the results -- here, we print the books
+            if (resultSet.next()) {
+                String dbuser = resultSet.getString("username");
+                String dbhash = resultSet.getString("password");
+                
+                // Close the connection
+                resultSet.close();
+                connection.close();
+                
+                System.out.println(dbuser+ " autenticated with pass: "+dbhash);
+                session.setAttribute("userauthenticated", dbuser);
+                
+                return "redirect:/users";
+            } else {
+                System.out.println(username+ " autenticated failed!");
+                
+                // Close the connection
+                resultSet.close();
+                connection.close();
+                
+                return "redirect:/adminlogin?error";
+            }
+        } catch (SQLException ex) {
+            return "redirect:/adminlogin?error";
+        }
     }
     
 }
